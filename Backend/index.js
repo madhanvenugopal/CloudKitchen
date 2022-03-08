8c7a23d01810945e3529906122d6aa9e1c8f87d8
madhanvenugopal <madhanvenugopal>
2022-03-08 19:53:19 +0530

    c

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-console.log("mathiten daww!");
-console.log("second statament");
-=======
-console.log("work agudhanu paaru!");
->>>>>>> f34f8c0016bca4b2f3ddc63a3f75fc8f8711680b
+console.log("hello world");
